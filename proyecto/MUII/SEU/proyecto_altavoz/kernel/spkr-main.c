60c1a60484bfebd7e8c8cb724da693c51ecd694c
Guille López <guilogar42@gmail.com>
2023-01-18 18:39:31 +0100

    fase 4 ahora sí que sí

--- proyecto/MUII/SEU/proyecto_altavoz/kernel/spkr-main.c
+++ proyecto/MUII/SEU/proyecto_altavoz/kernel/spkr-main.c
@@ -23,8 +23,9 @@ extern void spkr_on(void);
 extern void spkr_off(void);
 
 // variables de dispositivo
-static int minor = 0;
-static int major;
+static int spkr_minor = 0;
+static int spkr_major;
+static int buffersize = 0;
 static dev_t id_disp;
 static struct cdev disp;
 static struct class *class;
@@ -40,13 +41,17 @@ wait_queue_head_t lista_bloq;
 // temporizador
 static struct timer_list timer;
 
+// FIFO
+// static struct kfifo cola;
+
 // sonidos incompletos
 static char incomplete_sound[4];
 static int incomplete_count = 0;
 static int write_incomplete = 0;
 static int bytes_read = 0;
 
-module_param(minor, int, S_IRUGO);
+module_param(spkr_minor, int, S_IRUGO);
+module_param(buffersize, int, S_IRUGO);
 
 // **** Rutina que se activa al finalizar el temporizador ****
 void timer_function(struct timer_list *t)
@@ -104,37 +109,29 @@ static ssize_t spkr_write(struct file *filp, const char __user *buf, size_t coun
         return -ERESTARTSYS;
     }
 
-    // si es una escritura incompleta
-    if (count > 0 && count < 4)
+    bytes_read = 0;
+    total = count;
+
+    // si ha habido anteriormente una escritura incompleta
+    if (incomplete_count > 0)
     {
-        // guardamos los bytes
-        if (copy_from_user(incomplete_sound + incomplete_count, buf, count))
+        // guardamos los bytes incompletos
+        if (copy_from_user(incomplete_sound + incomplete_count, buf, 4 - incomplete_count))
         {
             mutex_unlock(&write_mutex);
             return -EFAULT;
         }
-        // guardamos el número de bytes leídos
-        incomplete_count += count;
 
-        // si ya tenemos todos los bytes marcamos que hay 4 bytes que escribir
-        if (incomplete_count == 4)
-        {
-            bytes_read = count;
-            count = 4;
-            incomplete_count = 0;
-            write_incomplete = 1;
-        }
-        // si no, se devuelve que se han leído los bytes incompletos
-        else
-        {
-            mutex_unlock(&write_mutex);
-            return count;
-        }
+        // marcamos que hay una escritura completa que estaba incompleta
+        write_incomplete = 1;
+
+        // guardamos el número de bytes incompletos que se han leído
+        bytes_read = 4 - incomplete_count;
+        incomplete_count = 0;
     }
 
-    total = (write_incomplete) ? bytes_read : count;
-    // si hay al menos 4 bytes que escribir
-    while (count >= 4)
+    // si hay al menos 4 bytes que escribir o una escritura anterior incompleta
+    while (count >= 4 || write_incomplete)
     {
         // miramos si ha habido una escritura incompleta que hay que hacer
         if (write_incomplete)
@@ -148,39 +145,64 @@ static ssize_t spkr_write(struct file *filp, const char __user *buf, size_t coun
         else
         {
             // extraemos sonido y frecuencia
-            if (get_user(time, (u_int16_t __user *)buf) || get_user(frequency, (u_int16_t __user *)(buf + 2)))
+            if (get_user(time, (u_int16_t __user *)(buf)) || get_user(frequency, (u_int16_t __user *)(buf + 2)))
             {
                 mutex_unlock(&write_mutex);
                 return -EFAULT;
             }
+            bytes_read = 4;
         }
 
-        printk(KERN_INFO "spkr set timer: %d\n", time);
-
-        // programamos frecuencia en dispositivo
-        if (frequency != 0)
+        // ESCRITURA CON BUFFER
+        if (buffersize != 0)
         {
-            set_spkr_frequency(frequency);
-            spkr_on();
         }
+        // ESCRITURA SIN BUFFER
+        else
+        {
+            printk(KERN_INFO "spkr set timer: %d\n", time);
+            printk(KERN_INFO "bytes: %d\n", bytes_read);
 
-        // arrancamos temporizador
-        timer_setup(&timer, timer_function, 0);
-        mod_timer(&timer, jiffies + msecs_to_jiffies(time));
+            // programamos frecuencia en dispositivo
+            if (frequency != 0)
+            {
+                set_spkr_frequency(frequency);
+                spkr_on();
+            }
 
-        // bloqueamos proceso
-        is_blocked = 0;
-        if (wait_event_interruptible(lista_bloq, is_blocked))
+            // arrancamos temporizador
+            timer_setup(&timer, timer_function, 0);
+            mod_timer(&timer, jiffies + msecs_to_jiffies(time));
+
+            // bloqueamos proceso
+            is_blocked = 0;
+            if (wait_event_interruptible(lista_bloq, is_blocked))
+            {
+                mutex_unlock(&write_mutex);
+                return -ERESTARTSYS;
+            }
+
+            spkr_off(); 
+            
+            // actualizamos valores
+            count -= bytes_read; // bytes leídos
+            buf += bytes_read;   // puntero del buffer de usuario
+        }
+    }
+
+    // si la escritura ha sido incompleta
+    if (count > 0 && count < 4)
+    {
+        // se guardan los bytes incompletos para completarlos posteriormente
+        if (copy_from_user(incomplete_sound + incomplete_count, buf, count))
         {
             mutex_unlock(&write_mutex);
-            return -ERESTARTSYS;
+            return -EFAULT;
         }
-
-        spkr_off();
-
-        // actualizamos valores
-        count -= 4; // bytes leídos
-        buf += 4;   // puntero del buffer de usuario
+        // guardamos el número de bytes leídos
+        incomplete_count += count;
+        count = 0;
+        buf += count;
     }
 
     mutex_unlock(&write_mutex);
@@ -204,16 +226,16 @@ static int init_spkr(void)
     spin_lock_init(&lock_timer_function);
 
     // Reserva de números major y minor
-    if (alloc_chrdev_region(&id_disp, minor, 1, DISP_NAME))
+    if (alloc_chrdev_region(&id_disp, spkr_minor, 1, DISP_NAME))
     {
         printk(KERN_ERR "Error en reserva de números major y minor del dispositivo.");
         return -1;
     }
 
     // Mostramos major seleccionado
-    major = MAJOR(id_disp);
-    printk(KERN_INFO "MAJOR: %d\n", major);
-    printk(KERN_INFO "MINOR: %d\n", minor);
+    spkr_major = MAJOR(id_disp);
+    printk(KERN_INFO "MAJOR: %d\n", spkr_major);
+    printk(KERN_INFO "MINOR: %d\n", spkr_minor);
 
     // Inicialización de parámetros de dispositivo y funciones
     cdev_init(&disp, &spkr_fops);
@@ -229,6 +251,12 @@ static int init_spkr(void)
     // Alta de un dispositivo para las aplicaciones
     class = class_create(THIS_MODULE, "speaker");
     device_create(class, NULL, id_disp, NULL, DISP_NAME);
+
+    // // creamos cola FIFO si hay buffersize
+    // if (buffersize)
+    // {
+    //     kfifo_alloc(&cola, buffersize, GFP_KERNEL);
+    // }
 
     printk(KERN_INFO "Device created\n");
 
